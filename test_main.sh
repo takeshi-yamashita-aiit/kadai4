1b9d8abc21609199aaf94e3a40207f635a59fc43
a2335ty <a2335ty@aiit.ac.jp>
2023-08-01 15:58:25 +0000

    first test

---
+++ test_main.sh
@@ -0,0 +1,17 @@
+# ------------------------------
+# 正常動作時のチェック
+# 最大公約数が計算できているか
+# 正常動作の正解データ
+echo "2" > /tmp/$$-ans
+# 正常動作の出力
+./main.sh 2 4 > /tmp/$$-result
+diff /tmp/$$-ans /tmp/$$-result || echo "テスト2-1" >> /tmp/$$-error.log
+
+
+# ------------------------------
+# エラーメッセージを出力する
+if [ -f /tmp/$$-error.log ];then
+        cat /tmp/$$-error.log 1>&2
+        rm -f /tmp/$$-*
+        exit 1
+fi
